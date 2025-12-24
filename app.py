04b3e0515504ce9bf91ba91535bdcccc4a6485c5
Himanshu Kumar <hk062848@gmail.com>
2025-12-25 03:26:03 +0530

    added

---
+++ app.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import date, datetime
+import os
+
+def send_email(subject, body):
+    email = os.environ["EMAIL_ADDRESS"]
+    password = os.environ["EMAIL_PASSWORD"]
+    to_email = os.environ["TO_EMAIL"]
+
+    msg = MIMEMultipart()
+    msg["From"] = email
+    msg["To"] = to_email
+    msg["Subject"] = subject
+
+    msg.attach(MIMEText(body, "plain"))
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(email, password)
+        server.send_message(msg)
+
+def main():
+    df = pd.read_csv("words.csv")
+
+    start_date = datetime.strptime(
+        os.environ["START_DATE"], "%Y-%m-%d"
+    ).date()
+
+    today = date.today()
+    day_number = (today - start_date).days + 1
+
+    row = df[df["day"] == day_number]
+
+    if row.empty:
+        print("No word found for today")
+        return
+
+    word = row.iloc[0]
+
+    subject = f"🗣️ Word of the Day – {word['word']}"
+
+    body = f"""
+Word: {word['word']}
+
+Meaning:
+{word['meaning']}
+
+Example:
+"{word['example']}"
+
+Try using this word today 🙂
+"""
+
+    send_email(subject, body)
+    print("Email sent successfully")
+
+if __name__ == "__main__":
+    main()
